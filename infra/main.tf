715da122abbc5a1c323ae73f231118294447d255
NagaPrasannaKasu <nagaprasannakasu2@gmail.com>
2024-12-07 14:45:53 +0100

    updated .tf files and .html files

--- infra/main.tf
+++ infra/main.tf
@@ -1,94 +1,195 @@
-resource "aws_lambda_function" "myfunc" {
-  filename         = data.archive_file.zip_the_python_code.output_path
-  source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
-  function_name    = "func"
-  role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "func.lambda_handler"
-  runtime          = "python3.8"
+# DynamoDB Table
+resource "aws_dynamodb_table" "visitor_count_ddb" {
+  name         = "Visitors_Table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "visitors"
+    type = "N"
+  }
+
+  global_secondary_index {
+    name            = "visitors_count"
+    hash_key        = "visitors"
+    projection_type = "ALL"
+    read_capacity   = 1
+    write_capacity  = 1
+  }
+
+  tags = {
+    Name = "Cloud Resume Challenge"
+  }
 }
 
+# DynamoDB Table Item
+resource "aws_dynamodb_table_item" "visitor_count_ddb" {
+  table_name = aws_dynamodb_table.visitor_count_ddb.name
+  hash_key   = aws_dynamodb_table.visitor_count_ddb.hash_key
 
+  item = <<ITEM
+{
+  "id": {"S": "visitor_count"},
+  "visitors": {"N": "1"}
+}
+ITEM
+}
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
+# Retrieve the current AWS region dynamically
+data "aws_region" "current" {}
 
+# Retrieve the current AWS account ID dynamically
+data "aws_caller_identity" "current" {}
+
+# create lambda iam role
+resource "aws_iam_role" "lambda_role" {
+  name               = "aws_resume_lamnbda_role"
   assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }    
-  ]
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
 }
 EOF
 }
 
-resource "aws_iam_policy" "iam_policy_for_resume_project" {
-
-  name        = "aws_iam_policy_for_terraform_resume_project_policy"
+# create lambda iam role policy
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+  name        = "iam_policy_for_aws_resume_lamnbda_role"
   path        = "/"
-  description = "AWS IAM Policy for managing the resume project role"
-    policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Action" : [
-            "logs:CreateLogGroup",
-            "logs:CreateLogStream",
-            "logs:PutLogEvents"
-          ],
-          "Resource" : "arn:aws:logs:*:*:*",
-          "Effect" : "Allow"
-        },
-        {
-          "Effect" : "Allow",
-          "Action" : [
-            "dynamodb:BatchGetItem",
-            "dynamodb:GetItem",
-            "dynamodb:Query",
-            "dynamodb:Scan",
-            "dynamodb:BatchWriteItem",
-            "dynamodb:PutItem",
-            "dynamodb:UpdateItem",
-            "dynamodb:*"
-          ],
-          "Resource" : "arn:aws:dynamodb:*:*:table/Table_VisitorsCount"
-        },
-      ]
-  })
+  description = "AWS IAM Policy for aws lambda to access dynamodb"
+  policy      = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Effect": "Allow",
+     "Action": [
+       "dynamodb:BatchGetItem",
+       "dynamodb:GetItem",
+       "dynamodb:Query",
+       "dynamodb:Scan",
+       "dynamodb:BatchWriteItem",
+       "dynamodb:PutItem",
+       "dynamodb:UpdateItem"
+     ],
+     "Resource": "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*" 
+   },
+   {
+     "Effect": "Allow",
+     "Action": [
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*" 
+   },
+   {
+     "Effect": "Allow",
+     "Action": "logs:CreateLogGroup",
+     "Resource": "*"
+   }
+ ]
+}
+EOF
 }
 
-
-
+# iam role policy attachment to lambda role
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-  role = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.iam_policy_for_resume_project.arn
-  
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
 
+# Archive the lambda function python code 
 data "archive_file" "zip_the_python_code" {
   type        = "zip"
-  source_file = "${path.module}/lambda/func.py"
+  source_dir  = "${path.module}/lambda/"
   output_path = "${path.module}/lambda/func.zip"
 }
 
-resource "aws_lambda_function_url" "url1" {
-  function_name      = aws_lambda_function.myfunc.function_name
-  authorization_type = "NONE"
-
-  cors {
-    allow_credentials = false
-    allow_origins     = ["*"]
-    allow_methods     = ["*"]
-    allow_headers     = ["date", "keep-alive"]
-    expose_headers    = ["keep-alive", "date"]
-    max_age           = 86400
+# lambda function creation
+resource "aws_lambda_function" "lambda_func" {
+  filename      = "${path.module}/lambda/func.zip"
+  function_name = "VistorCounter"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "func.lambda_handler"
+  runtime       = "python3.8"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  environment {
+    variables = {
+      databaseName = "Visitors_Table"
+    }
   }
+
+}
+
+# create api gateway
+resource "aws_apigateway_api" "visitor_counter_api" {
+  name          = "visitor_counter_http_api"
+  protocol_type = "HTTP"
+  description   = "Visitor counter HTTP API to invoke AWS Lambda function to update & retrieve the visitors count"
+  cors_configuration {
+      allow_credentials = false
+      allow_headers     = []
+      allow_methods     = [
+          "GET",
+          "OPTIONS",
+          "POST",
+      ]
+      allow_origins     = [
+          "*",
+      ]
+      expose_headers    = []
+      max_age           = 0
+  }
+
+}
+
+# api gateway stage
+resource "aws_apigateway_stage" "default" {
+  api_id = aws_apigateway_api.visitor_counter_api.id
+  name        = "default"
+  auto_deploy = true
+}
+
+# api gateway integration
+resource "aws_apigateway_integration" "visitor_counter_api_integration" {
+  api_id = aws_apigateway_api.visitor_counter_api.id
+  integration_uri    = aws_lambda_function.lambda_func.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+# api gateway route
+resource "aws_apigateway_route" "any" {
+  api_id = aws_apigateway_api.visitor_counter_api.id
+  route_key = "ANY /VisitorCounter"
+  target    = "integrations/${aws_apigateway_integration.visitor_counter_api_integration.id}"
+}
+
+# api gateway lambda invocation permission
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_func.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigateway_api.visitor_counter_api.execution_arn}/*/*"
+}
+
+output "base_url" {
+  value = "${aws_apigateway_stage.default.invoke_url}/VisitorCounter"
 }
